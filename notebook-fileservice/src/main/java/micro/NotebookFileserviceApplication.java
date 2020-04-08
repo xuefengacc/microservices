deb3daf43692bd2a364d61a62e00b92131c513b6
xuefengacc <xuefeng.acc@gmail.com>
2020-04-07 22:30:55 -0400

    Create file service

---
+++ notebook-fileservice/src/main/java/micro/NotebookFileserviceApplication.java
@@ -0,0 +1,13 @@
+package micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebookFileserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotebookFileserviceApplication.class, args);
+	}
+
+}
---
+++ notebook-fileservice/src/test/java/micro/NotebookFileserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package micro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebookFileserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
